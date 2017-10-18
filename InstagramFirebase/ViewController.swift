a5137b1f36670f0982b800e6dfd501122fea4224
Christian Deciga <christian@everipedia.com>
2017-10-18 14:24:20 -0700

    imported image asssets to project, created button with constraints programatically

--- InstagramFirebase/ViewController.swift
+++ InstagramFirebase/ViewController.swift
@@ -10,15 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //create button programmatically
+    let plusPhotoButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        button.translatesAutoresizingMaskIntoConstraints = false //use this for constraints programatically
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(plusPhotoButton)
+        
+        //anchors for auto-layout
+        plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true //required for autolayout
+        
+        plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
