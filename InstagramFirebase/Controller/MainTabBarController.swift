c4782bab5e27ceaa418f10c91e0bea6f9a2b2f71
Christian Deciga <christian@everipedia.com>
2017-10-22 15:06:59 -0700

    created MainTabBarController.swift

---
+++ InstagramFirebase/Controller/MainTabBarController.swift
@@ -0,0 +1,9 @@
+//
+//  MainTabBarController.swift
+//  InstagramFirebase
+//
+//  Created by everipedia_iMac on 10/22/17.
+//  Copyright © 2017 Christian Deciga. All rights reserved.
+//
+
+import Foundation
