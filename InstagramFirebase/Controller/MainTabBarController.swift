dabfc5cecd23ef713741294cf7477dd04fa84679
Christian Deciga <christian@everipedia.com>
2017-10-22 15:51:22 -0700

    changed UserProfileController to UICollectionViewController

--- InstagramFirebase/Controller/MainTabBarController.swift
+++ InstagramFirebase/Controller/MainTabBarController.swift
@@ -13,12 +13,12 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        let redVC = UIViewController()
-        redVC.view.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        //collectionview
+        let layout = UICollectionViewFlowLayout()
+        let userProfileController = UserProfileController(collectionViewLayout: layout)
         
         //programmatically build navigation controller
-        let navController = UINavigationController(rootViewController: redVC)
+        let navController = UINavigationController(rootViewController: userProfileController)
         
         viewControllers = [navController]
     }
--- InstagramFirebase/Controller/UserProfileController.swift
+++ InstagramFirebase/Controller/UserProfileController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
-class UserProfileController: UIViewController {
+class UserProfileController: UICollectionViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+    }
 }
