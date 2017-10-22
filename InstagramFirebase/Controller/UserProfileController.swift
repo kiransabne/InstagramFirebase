95f8d5e8559ed688f8bfa15e57c4c854bf7dcfae
Christian Deciga <christian@everipedia.com>
2017-10-22 15:41:28 -0700

    created UserProfileController

---
+++ InstagramFirebase/Controller/UserProfileController.swift
@@ -0,0 +1,13 @@
+//
+//  UserProfileController.swift
+//  InstagramFirebase
+//
+//  Created by everipedia_iMac on 10/22/17.
+//  Copyright © 2017 Christian Deciga. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileController: UIViewController {
+    
+}
