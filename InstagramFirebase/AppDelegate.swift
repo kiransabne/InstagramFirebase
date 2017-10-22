071f1981fd5b44eaa02b6a84c72c2aafe5179ddb
Christian Deciga <christian@everipedia.com>
2017-10-22 15:22:21 -0700

    created navController programmatically

--- InstagramFirebase/AppDelegate.swift
+++ InstagramFirebase/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         //programmatically build window
         window = UIWindow()
-        window?.rootViewController = UITabBarController()
+        window?.rootViewController = MainTabBarController()
         
         
         
--- InstagramFirebase/Controller/MainTabBarController.swift
+++ InstagramFirebase/Controller/MainTabBarController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        let redVC = UIViewController()
+        redVC.view.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        
+        //programmatically build navigation controller
+        let navController = UINavigationController(rootViewController: redVC)
+        
+        viewControllers = [navController]
     }
     
     
