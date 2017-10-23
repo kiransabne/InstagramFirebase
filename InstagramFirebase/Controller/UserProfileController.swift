82cbc79932f21621d30335fd69a7582ade57cf47
Christian Deciga <christian@everipedia.com>
2017-10-23 01:54:29 -0700

    set up fetching user's username and displayed it at top of UINavigationBar

--- InstagramFirebase/Controller/UserProfileController.swift
+++ InstagramFirebase/Controller/UserProfileController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Firebase
 
 class UserProfileController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView?.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        collectionView?.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        navigationItem.title = "User Profile"
+        
+        
     }
 }
