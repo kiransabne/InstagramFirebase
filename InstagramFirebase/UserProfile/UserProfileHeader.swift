8be2cedc02bd1da1fc6633d0c8c74b5e959e4b73
Christian Deciga <christian@everipedia.com>
2017-10-23 02:58:14 -0700

    created UserProfile group, added UserProfileHeader.swift

---
+++ InstagramFirebase/UserProfile/UserProfileHeader.swift
@@ -0,0 +1,22 @@
+//
+//  UserProfileHeader.swift
+//  InstagramFirebase
+//
+//  Created by everipedia_iMac on 10/23/17.
+//  Copyright © 2017 Christian Deciga. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileHeader: UICollectionViewCell {
+    
+    //UIView objects subclass
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
