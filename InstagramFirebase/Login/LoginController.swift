346159fd8c0c169a7c75a72ff48ac2aeda23c02e
Christian Deciga <christian@everipedia.com>
2017-10-30 00:26:42 -0700

    created LoginController in Login folder

---
+++ InstagramFirebase/Login/LoginController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginController.swift
+//  InstagramFirebase
+//
+//  Created by everipedia_iMac on 10/30/17.
+//  Copyright © 2017 Christian Deciga. All rights reserved.
+//
+
+import UIKit 
