4a3ce4c511765a1508c7f47b859b540aca3907f3
Christian Deciga <christian@everipedia.com>
2017-11-03 23:19:34 -0700

    started PhotoSelectorCell.swift in AddPhoto group

---
+++ InstagramFirebase/AddPhoto/PhotoSelectorCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoSelectorCell.swift
+//  InstagramFirebase
+//
+//  Created by everipedia_iMac on 11/3/17.
+//  Copyright © 2017 Christian Deciga. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorCell: UICollectionViewCell {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
