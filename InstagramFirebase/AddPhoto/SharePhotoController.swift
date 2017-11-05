38bd0e0a6f0d5cadd625ac674fa1cd64c739a9ed
Christian Deciga <christian@everipedia.com>
2017-11-05 00:19:51 -0700

    started container view

--- InstagramFirebase/AddPhoto/SharePhotoController.swift
+++ InstagramFirebase/AddPhoto/SharePhotoController.swift
@@ -14,7 +14,7 @@ class SharePhotoController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        view.backgroundColor = #colorLiteral(red: 0.9567589164, green: 0.9569225907, blue: 0.9567485452, alpha: 1)
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share", style: .plain, target: self, action: #selector(handleShare)) //create Share bar button item
         
@@ -23,6 +23,13 @@ class SharePhotoController: UIViewController {
     
     fileprivate func setupImageAndTextViews() {
         //setup image and text views in this method
+        
+        let containerView = UIView()
+        containerView.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        //add containerview with constraints
+        view.addSubview(containerView)
+        containerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 100)
     }
     
     
