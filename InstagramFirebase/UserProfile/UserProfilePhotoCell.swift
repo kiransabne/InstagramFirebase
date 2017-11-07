a8300d87485db9cd8b28cc86cfcadfb64d6cacda
Christian Deciga <christian@everipedia.com>
2017-11-07 01:45:42 -0800

    created let photoImageView in UserProfilePhotoCell

--- InstagramFirebase/UserProfile/UserProfilePhotoCell.swift
+++ InstagramFirebase/UserProfile/UserProfilePhotoCell.swift
@@ -11,4 +11,24 @@ import UIKit
 
 class UserProfilePhotoCell: UICollectionViewCell {
     
+    //image view to load image in
+    let photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+        
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
