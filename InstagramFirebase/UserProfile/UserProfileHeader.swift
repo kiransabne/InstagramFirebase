c13a4924f6f104676988e469b3acf941e6fbc2c6
Christian Deciga <christian@everipedia.com>
2017-11-09 01:01:30 -0800

    deleted previous func

--- InstagramFirebase/UserProfile/UserProfileHeader.swift
+++ InstagramFirebase/UserProfile/UserProfileHeader.swift
@@ -194,32 +194,6 @@ class UserProfileHeader: UICollectionViewCell {
     }
     
     
-    //setup profile image view
-    
-    fileprivate func setupProfileImage() {
-        guard let profileImageUrl = user?.profileImageUrl else { return }
-        
-        guard let url = URL(string: profileImageUrl) else { return }
-        
-        URLSession.shared.dataTask(with: url) { (data, response, err) in
-            //check for the error, then construct the image using data
-            if let err = err {
-                print("Failed to fetch profile image: ", err)
-                return
-            }
-            
-            //perhaps check for response status of 200 (HTTP OK)
-            guard let data = data else { return }
-            
-            let image = UIImage(data: data)
-            
-            //need to get back onto the main UI Thread
-            DispatchQueue.main.async {
-                self.profileImageView.image = image
-            }
-            
-        }.resume()
-    }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
