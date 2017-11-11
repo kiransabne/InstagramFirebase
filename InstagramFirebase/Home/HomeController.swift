8fcd30d8f67c830655e27ce8ef935b33e6ee0e87
Christian Deciga <christian@everipedia.com>
2017-11-10 20:19:19 -0800

    started building out collectionview cells in HomeController.swift

--- InstagramFirebase/Home/HomeController.swift
+++ InstagramFirebase/Home/HomeController.swift
@@ -10,12 +10,32 @@ import UIKit
 
 class HomeController: UICollectionViewController {
     
+    let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView?.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        
+        
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    //render out items in collection view
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
     }
     
     
-    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        //get cell from collectionview by dequeue
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+         cell.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+         return cell
+       
+        
+        
+    }
     
 }
