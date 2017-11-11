d280d519667965460e122d54c27d142e05d64296
Christian Deciga <christian@everipedia.com>
2017-11-10 21:16:52 -0800

    Created HomePostCell.swift

---
+++ InstagramFirebase/Home/HomePostCell.swift
@@ -0,0 +1,26 @@
+//
+//  HomePostCell.swift
+//  InstagramFirebase
+//
+//  Created by everipedia_iMac on 11/10/17.
+//  Copyright © 2017 Christian Deciga. All rights reserved.
+//
+
+import UIKit
+
+//new cell renders out entire post
+
+class HomePostCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    
+}
