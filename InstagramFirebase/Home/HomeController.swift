77fc533f0fc44b0c8566df15e5e07a5c06c37276
Christian Deciga <christian@everipedia.com>
2017-11-13 03:53:02 -0800

    fetched username to display in vertical list posts succesfully

--- InstagramFirebase/Home/HomeController.swift
+++ InstagramFirebase/Home/HomeController.swift
@@ -31,33 +31,44 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
         
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
-        let ref = Database.database().reference().child("posts").child(uid)
-        
-        ref.observeSingleEvent(of: .value, with: { (snapshot) in
-            //print(snapshot.value)
+        //fetch user via Firebase database call to show username
+        Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
             
-            guard let dictionaries = snapshot.value as? [String: Any] else { return }
+            guard let userDictionary = snapshot.value as? [String: Any] else { return }
             
-            dictionaries.forEach({ (key, value) in
-                //print("Key\(key), Value: \(value)")
-                
-                guard let dictionary = value as? [String: Any] else { return }
-                
-                let dummyUser = User(dictionary: ["username": "bvoong"])
-                
-                
-                let post = Post(user: dummyUser, dictionary: dictionary)
-                
-                
-                self.posts.append(post)
-                
-            })
+            let user = User(dictionary: userDictionary) //user object
             
-            self.collectionView?.reloadData()
+            let ref = Database.database().reference().child("posts").child(uid)
             
+            ref.observeSingleEvent(of: .value, with: { (snapshot) in
+                //print(snapshot.value)
+                
+                guard let dictionaries = snapshot.value as? [String: Any] else { return }
+                
+                dictionaries.forEach({ (key, value) in
+                    //print("Key\(key), Value: \(value)")
+                    
+                    guard let dictionary = value as? [String: Any] else { return }
+                    
+                    
+                    let post = Post(user: user, dictionary: dictionary)
+                    
+                    
+                    self.posts.append(post)
+                    
+                })
+                
+                self.collectionView?.reloadData()
+                
+            }) { (err) in
+                print("Failed to fetch posts:", err)
+            }
         }) { (err) in
-            print("Failed to fetch posts:", err)
+            print("Failed to fetch user for posts:", err)
         }
+        
+        
+        
     }
     
     
