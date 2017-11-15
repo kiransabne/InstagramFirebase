35c8c1ba01756a9f5ce22875466fdd70dee13bc1
Christian Deciga <christian@everipedia.com>
2017-11-15 01:49:26 -0800

    created searchbar with anchor constraints

--- InstagramFirebase/Search/UserSearchController.swift
+++ InstagramFirebase/Search/UserSearchController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class UserSearchController: UICollectionViewController {
     
+    //create search bar
+    let searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.placeholder = "Enter username"
+        return sb
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView?.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        
+        
+        navigationController?.navigationBar.addSubview(searchBar)
+        let navBar = navigationController?.navigationBar
+        searchBar.anchor(top: navBar?.topAnchor, left: navBar?.leftAnchor, bottom: navBar?.bottomAnchor, right: navBar?.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
     }
     
     
