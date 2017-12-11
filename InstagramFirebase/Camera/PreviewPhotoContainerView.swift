68dc94ef8da6a106c45ce6e9703432e8211882df
Christian Deciga <christian@everipedia.com>
2017-12-11 03:45:13 -0800

    created PreviewPhotoContainerView.swift in Camera group

---
+++ InstagramFirebase/Camera/PreviewPhotoContainerView.swift
@@ -0,0 +1,9 @@
+//
+//  PreviewPhotoContainerView.swift
+//  InstagramFirebase
+//
+//  Created by everipedia_iMac on 12/11/17.
+//  Copyright © 2017 Christian Deciga. All rights reserved.
+//
+
+import Foundation
