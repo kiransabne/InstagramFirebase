b62659a62f48c8d9bf4405704a77d95ad5d3bb5b
Christian Deciga <christian@everipedia.com>
2017-12-11 03:48:47 -0800

    started implementing container view

--- InstagramFirebase/Camera/PreviewPhotoContainerView.swift
+++ InstagramFirebase/Camera/PreviewPhotoContainerView.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2017 Christian Deciga. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PreviewPhotoContainerView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
