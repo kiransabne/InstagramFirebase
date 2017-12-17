b686ce80066ed8742b48c7f14300e2bb888a2633
Christian Deciga <christian@everipedia.com>
2017-12-17 03:19:23 -0800

    added action to submitButton in CommentsController.swift, updated anchored constraints

--- InstagramFirebase/Home/CommentsController.swift
+++ InstagramFirebase/Home/CommentsController.swift
@@ -49,6 +49,7 @@ class CommentsController: UICollectionViewController {
         submitButton.setTitle("Submit", for: .normal)
         submitButton.setTitleColor(.black, for: .normal)
         submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        submitButton.addTarget(self, action: #selector(handleSubmit), for: .touchUpInside) //addtarget to submit button
         containerView.addSubview(submitButton)
         submitButton.anchor(top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 50, height: 0)
         
@@ -56,11 +57,17 @@ class CommentsController: UICollectionViewController {
         let textField = UITextField()
         textField.placeholder = "Enter Comment"
         containerView.addSubview(textField)
-        textField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        textField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: submitButton.leftAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
         
         return containerView
         
     }()
+    
+    //action for submit button
+    @objc func handleSubmit() {
+        print("Handling submit...")
+        
+    }
     
     //accessory view property to show bottom portion of comments page
     override var inputAccessoryView: UIView? {
