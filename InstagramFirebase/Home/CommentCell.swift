948697b7a20b5899a0d6485e464352ed5eb6e77a
Christian Deciga <christian@everipedia.com>
2017-12-19 02:21:54 -0800

    rendered profileImageView to round

--- InstagramFirebase/Home/CommentCell.swift
+++ InstagramFirebase/Home/CommentCell.swift
@@ -44,6 +44,7 @@ class CommentCell: UICollectionViewCell {
         
         addSubview(profileImageView)
         profileImageView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 8, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 40, height: 40)
+        profileImageView.layer.cornerRadius = 40 / 2 //make profile imaage rounded 
         
         addSubview(textLabel)
         textLabel.anchor(top: topAnchor, left: profileImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 4, paddingBottom: 4, paddingRight: 4, width: 0, height: 0) //render inside of cell
