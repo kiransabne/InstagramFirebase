956ca268b5c6f0d7c400de1a5fc5ab2fd73026d8
Christian Deciga <christian@everipedia.com>
2017-12-19 02:14:29 -0800

    started rendering profileImageview in CommentCell.swift

--- InstagramFirebase/Home/CommentCell.swift
+++ InstagramFirebase/Home/CommentCell.swift
@@ -26,12 +26,13 @@ class CommentCell: UICollectionViewCell {
         return label
     }()
     
-    //
+    //profile image to add to collectionview cells
     let profileImageView: CustomImageView = {
         
         let iv = CustomImageView()
         iv.clipsToBounds = true
-        iv.contentMode = .scaleAspectFill 
+        iv.contentMode = .scaleAspectFill
+        iv.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
         return iv
         
     }()
@@ -40,8 +41,12 @@ class CommentCell: UICollectionViewCell {
         super.init(frame: frame)
         
         backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 8, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 40, height: 40)
+        
         addSubview(textLabel)
-        textLabel.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 4, paddingBottom: 4, paddingRight: 4, width: 0, height: 0) //render inside of cell
+        textLabel.anchor(top: topAnchor, left: profileImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 4, paddingBottom: 4, paddingRight: 4, width: 0, height: 0) //render inside of cell
     }
     
     required init?(coder aDecoder: NSCoder) {
